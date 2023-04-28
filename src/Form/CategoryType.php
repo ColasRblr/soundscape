5c562ce265879ec4be89bcb13b5449e9da790f21
Hanaline <gaelle.boutolini.simplon@gmail.com>
2023-04-28 16:58:34 +0200

    push afin de merges toutes les parties du projet

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -11,7 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategoryType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options, Security $security): void
+    public function buildForm(FormBuilderInterface $builder, array $options, ): void
     {
         $builder
             ->add('name')
